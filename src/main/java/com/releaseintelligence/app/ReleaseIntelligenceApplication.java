aecca48e08c5b2f617a86e69f917e8095ec1f3db
Release Inteligence <releaseinteligence@gmail.com>
2026-06-19 12:54:30 +0530

    Initial commit: Release Intelligence Spring Boot setup

---
+++ src/main/java/com/releaseintelligence/app/ReleaseIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.releaseintelligence.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/releaseintelligence/app/ReleaseIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.releaseintelligence.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
